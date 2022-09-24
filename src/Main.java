572515761abb723f6cc8735c66069595ce64226a
igor <igor.fernandes@mercadolivre.com>
2022-09-24 13:20:41 -0300

    exercicio 5 e 6

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Pessoa individuo1 = new Pessoa();
 
-        Pessoa individuo2 = new Pessoa("roberto", 34 , 3578);
+        Pessoa dadospessoal = new Pessoa("roberto", 34 , 3578);
 
-        Pessoa individuo3 = new Pessoa(74.75,1.75);
+        Pessoa caracteristicas = new Pessoa(74.75,1.75);
 
-        System.out.println(individuo1);
+
+        System.out.println(dadospessoal.toString());
+
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,8 +1,25 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Pessoa {
     String nome;
     int idade;
     int id;
     double peso, altura;
+
+   public void calcularIMC() {
+       double imc = this.peso/(Math.pow(this.altura, 2));
+       if (imc < 20 )  System.out.println("Abaixo do peso");
+       if (imc >= 20 && imc <= 25 )  System.out.println("Peso Saudável");
+       if (imc > 25 )  System.out.println("Sobrepeso");
+    }
+
+    public void ehMaiorIdade() {
+        if ((idade < 18)) {
+            System.out.println("menor");
+        } else {
+            System.out.println("menor");
+        }
+    }
 
     public Pessoa() {
         this.nome = nome;
@@ -19,5 +36,14 @@ public class Pessoa {
         this.altura = altura;
     }
 
-
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", id=" + id +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
 }

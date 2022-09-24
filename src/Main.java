cbc1c94b0f5bb36935ca825ef2912331a739da88
igor <igor.fernandes@mercadolivre.com>
2022-09-24 11:09:44 -0300

    cria os objetos apartir dos cronstructors exercicio 1 ao 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa individuo1 = new Pessoa();
+
+        Pessoa individuo2 = new Pessoa("roberto", 34 , 3578);
+
+        Pessoa individuo3 = new Pessoa(74.75,1.75);
+
+        System.out.println(individuo1);
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,23 @@
+public class Pessoa {
+    String nome;
+    int idade;
+    int id;
+    double peso, altura;
+
+    public Pessoa() {
+        this.nome = nome;
+    }
+
+    public Pessoa(String nome, int idade, int id) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+    }
+
+    public Pessoa(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+
+}
